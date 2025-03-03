6d101966c680bf2982d4bade2ce3d00a53ff0fe4
antoniovizarraga <antonio.vizarraga@iesnervion.es>
2025-03-03 14:08:10 +0100

    Creado vista y el botón redirige a la vista cómo está planeado

---
+++ JuegoUI/ViewModels/HomeVM.cs
@@ -0,0 +1,37 @@
+﻿using _18_CRUD_Personas_UWP_UI.ViewModels.Utilidades;
+using JuegoUI.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuegoUI.ViewModels
+{
+    class HomeVM
+    {
+        private DelegateCommand playCommand;
+
+
+        public DelegateCommand PlayCommand
+        {
+            get { return playCommand; }
+        }
+
+        public HomeVM()
+        {
+            playCommand = new DelegateCommand(PlayCommand_Executed);
+        }
+
+        private async void cambioPagina()
+        {
+            await Shell.Current.GoToAsync("//GamePage");
+        }
+
+
+        private void PlayCommand_Executed()
+        {
+            cambioPagina();
+        }
+    }
+}
---
+++ JuegoUI/Views/GamePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace JuegoUI.Views;
+
+public partial class GamePage : ContentPage
+{
+	public GamePage()
+	{
+		InitializeComponent();
+	}
+}
